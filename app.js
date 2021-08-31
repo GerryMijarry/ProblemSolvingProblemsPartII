1966cf99c4a27fc13c29485a5364f2e4a983ede6
GerryMijarry <89660119+GerryMijarry@users.noreply.github.com>
2021-08-31 09:59:56 -0600

    setup files and TODO comments

---
+++ app.js
@@ -0,0 +1,16 @@
+// TODO Happy Numbers
+// https://en.wikipedia.org/wiki/Happy_number
+// A happy number is a number defined by the following process: starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1. An example of a happy number is 19
+// Write a method that determines if a number is happy or sad
+
+// TODO Prime Numbers
+// A prime number is a number that is only divisible by one and itself.
+// Write a method that prints out all prime numbers between 1 and 100 
+
+// TODO Fibonacci
+// A series of numbers in which each number (Fibonacci number) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+// Write a method that does the Fibonacci sequence starting at 1
+
+
+
+// TODO HARDER VERSION: Write a method that does the Fibonacci sequence starting at a number that a user inputs
